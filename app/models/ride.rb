5b6da57ac1083539a35735ef85ecff37148f7efe
Kris Litman <duguy8@gmail.com>
2021-02-22 11:23:20 -0500

    All model tests passing. Working on user story 1

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,6 @@ class Ride < ApplicationRecord
   validates_presence_of :name,
                         :thrill_rating,
                         :open
+  has_many :mechanic_rides
+  has_many :mechanics, through: :mechanic_rides                      
 end
